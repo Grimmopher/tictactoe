74497a71dbd0d0adbe86c87d3740f5092c1aea5e
Chris Grimm <chris.grimm@gmail.com>
2018-12-10 16:23:07 -0500

    added the whole cmake directory, this is likely unncessary but I dont know how to rebuild it yet

---
+++ cmake-build-debug/CMakeFiles/myApp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myApp.dir/src/main.cpp.o"
+  "CMakeFiles/myApp.dir/src/Engine/Engine.cpp.o"
+  "CMakeFiles/myApp.dir/src/tictactoe/GameBoard.cpp.o"
+  "CMakeFiles/myApp.dir/src/Engine/Clock/Time.cpp.o"
+  "CMakeFiles/myApp.dir/src/Engine/Renderer.cpp.o"
+  "CMakeFiles/myApp.dir/src/Engine/Clock/Clock.cpp.o"
+  "myApp.pdb"
+  "myApp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
